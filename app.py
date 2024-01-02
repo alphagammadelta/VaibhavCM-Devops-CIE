b33b541074930f92530820944af447456f6ef3d4
alphagammadelta <96831466+alphagammadelta@users.noreply.github.com>
2024-01-02 12:08:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Srujan Vinod Sarode 1BM21IS411</h1>"
+    return "<h1>Vaibhav C M 1BM20IS174</h1>"
 
 
 if __name__ == "__main__":
